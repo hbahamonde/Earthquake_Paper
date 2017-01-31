b02a0554758eb63cfbd2793341fa0a94b373aecc
hbahamonde <bahamondeh@gmail.com>
2017-01-31 18:19:02 -0500

    both EQ time-series and EQ maps plots are ready.

--- Earthquake_Paper.R
+++ Earthquake_Paper.R
@@ -1,6 +1,38 @@
 cat("\014")
 rm(list=ls())
 
+############################
+#### Loadings
+############################
+
+# Load the earthquake data
+if (!require("pacman")) install.packages("pacman"); library(pacman)
+p_load(foreign)
+
+
+###### CHILE ##############
+dat.chile <- read.csv("/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Data/Chile_Data_Earthquake.csv")
+
+# Transformations
+dat.chile$ln.Magnitude = log(dat.chile$Magnitude)
+dat.chile$W.Deaths = (dat.chile$Deaths/dat.chile$Population)*100
+
+# save dataset
+save(dat.chile, file = "/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Chile_Data_Earthquake.RData")
+
+
+###### PERU ##############
+dat.peru <- read.csv("/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Data/Peru_Data_Earthquake.csv")
+
+
+dat.peru$Magnitude = as.numeric(as.character(dat.peru$Magnitude))
+dat.peru$Deaths = as.numeric(as.character(dat.peru$Deaths))
+dat.peru$W.Deaths = (dat.peru$Deaths/dat.peru$Population)*100
+
+
+# save dataset
+save(dat.peru, file = "/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Peru_Data_Earthquake.RData")
+
 
 ############################
 #### EARTHQUAKES PLOTS
@@ -12,14 +44,9 @@ rm(list=ls())
 
 # Load the earthquake data
 if (!require("pacman")) install.packages("pacman"); library(pacman)
-p_load(foreign,ggplot2)
+p_load(ggplot2)
 
-dat.chile <- read.csv("/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Data/Chile_Data_Earthquake.csv")
-
-# Transformations
-dat.chile$ln.Magnitude = log(dat.chile$Magnitude)
-dat.chile$W.Deaths = (dat.chile$Deaths/dat.chile$Population)*100
-
+load("/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Chile_Data_Earthquake.RData")
 
 eq.chile.p = ggplot(dat.chile, aes(x = year, y = Magnitude, size = W.Deaths)) +
         geom_point(shape = 21) +
@@ -32,21 +59,11 @@ eq.chile.p = ggplot(dat.chile, aes(x = year, y = Magnitude, size = W.Deaths)) +
 
 
 ## PERU
-
+load("/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Peru_Data_Earthquake.RData")
 
 # Load the earthquake data
 if (!require("pacman")) install.packages("pacman"); library(pacman)
-p_load(foreign,ggplot2)
-
-dat.peru <- read.csv("/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Data/Peru_Data_Earthquake.csv")
-
-
-dat.peru$Magnitude = as.numeric(as.character(dat.peru$Magnitude))
-dat.peru$Deaths = as.numeric(as.character(dat.peru$Deaths))
-
-# Transformations
-dat.peru$ln.Magnitude = log(dat.peru$Magnitude)
-dat.peru$W.Deaths = (dat.peru$Deaths/dat.peru$Population)*100
+p_load(ggplot2)
 
 
 eq.peru.p =ggplot(dat.peru, aes(x = year, y = Magnitude, size = W.Deaths)) +
@@ -56,9 +73,6 @@ eq.peru.p =ggplot(dat.peru, aes(x = year, y = Magnitude, size = W.Deaths)) +
         theme_bw() +
         ggtitle("Peru") +
         scale_size("Weighted Deaths")
-
-
-
 
 ### plot sharing the same legend
 if (!require("pacman")) install.packages("pacman"); library(pacman)
@@ -89,119 +103,68 @@ grid_arrange_shared_legend <- function(..., ncol = length(list(...)), nrow = 1, 
   
 }
 
-
-
 ### plot both countries
 grid_arrange_shared_legend(eq.chile.p, eq.peru.p, ncol = 1, nrow = 2)
 
 
-
-
 ############################
-#### MAPS
+#### MAPS: CHILE
 ############################
+cat("\014")
+rm(list=ls())
 
-
+## packages
 if (!require("pacman")) install.packages("pacman"); library(pacman)
-p_load(GISTools,rgdal,RColorBrewer,maptools,ggplot2,plyr,rgeos)
-
-municipalities <- readOGR(dsn = "/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Data/DPA_INE", layer = "Comuna")
-
-municipalities <- gSimplify(municipalities, tol=10000, topologyPreserve=T)
-
-# municipalities <- spTransform(municipalities, CRS("+proj=longlat +datum=WGS84"))
-municipalities <- fortify(municipalities)
-municipalities <- municipalities[!(municipalities$long <= -76),]
-
-
-ggplot() + 
-        geom_polygon(aes(x=long, y=lat, group=group), fill='grey', size=.05, color='black', data=municipalities, alpha=1/2) +
-        theme_bw() +
-        ggtitle("Chile")
-
-
-
-###
-
-## provincias
-if (!require("pacman")) install.packages("pacman"); library(pacman)
-p_load(GISTools,rgdal,RColorBrewer,maptools,ggplot2,plyr)
+p_load(rgdal, foreign, rgeos, ggplot2)
 
 
 # load shape file
-provinces <- readOGR(dsn = "/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Data/division_provincial", layer = "division_provincial")
+chile.provinces <- readOGR(dsn = "/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Data/CHL_adm_shp", layer = "CHL_adm2")
 
 # load eq data
-if (!require("pacman")) install.packages("pacman"); library(pacman)
-p_load(foreign)
-dat.chile <- read.csv("/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Data/Chile_Data_Earthquake.csv")
+load("/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Chile_Data_Earthquake.RData")
+dat.chile <- dat.chile[!(dat.chile$Longitude <= -76),]
 
 
-provinces <- gSimplify(provinces, tol=10000, topologyPreserve=T)
-provinces <- spTransform(provinces, CRS("+proj=longlat +datum=WGS84"))
-provinces <- fortify(provinces)
-provinces <- provinces[!(provinces$long <= -76),]
+#chile.provinces <- gSimplify(chile.provinces, tol=10000, topologyPreserve=T)
+chile.provinces <- spTransform(chile.provinces, CRS("+proj=longlat +datum=WGS84"))
+chile.provinces <- fortify(chile.provinces)
+chile.provinces <- chile.provinces[!(chile.provinces$long <= -76),]
 
-ggplot() + 
-        geom_polygon(aes(x=long, y=lat, group=group), fill='grey', size=.05, color='black', data=provinces, alpha=1/2) +
+
+ggplot() +  
+        geom_polygon(aes(x=long, y=lat, group=group), fill='grey', size=.05, color='black', data=chile.provinces, alpha=1/2) +
         theme_bw() +
-        ggtitle("Chile") +
-        geom_point(aes(x = Longitude, y = Latitude, size=Magnitude), shape=21, data = subset(dat.chile, year<1900) , alpha = .5, color="red")
+        ggtitle("Chile") + 
+        geom_point(data=subset(dat.chile, year>=1900), aes(x=Longitude, y=Latitude, size=Magnitude), color="red", shape=21)
 
 
+############################
+#### MAPS: Peru
+############################
+
+cat("\014")
+rm(list=ls())
+
+## packages
+if (!require("pacman")) install.packages("pacman"); library(pacman)
+p_load(rgdal, foreign, rgeos, ggplot2)
+
+# load eq data
+load("/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Peru_Data_Earthquake.RData")
+
+# load shape file
+peru.provinces <- readOGR(dsn = "/Users/hectorbahamonde/RU/Dissertation/Papers/Earthquake_Paper/Data/PER_adm_shp", layer = "PER_adm2")
 
 
-####
+#peru.provinces <- gSimplify(peru.provinces, tol=10000, topologyPreserve=T)
+peru.provinces <- spTransform(peru.provinces, CRS("+proj=longlat +datum=WGS84"))
+peru.provinces <- fortify(peru.provinces)
+#peru.provinces <- peru.provinces[!(peru.provinces$long <= -76),]
 
 
-
-
-
-
-
-
-
-
-
-####
-p_load(rgdal,raster,sp,rgeos)
-chile<-getData('GADM', country="CHL", level=0)
-
-poly2df <- function(poly) {
-        # Convert the polygons into data frames so we can make lines
-        # Number of regions
-        n_regions <- length(poly@polygons)
-        
-        # Get the coords into a data frame
-        poly_df <- c()
-        for(i in 1:n_regions) {
-                # Number of polygons for first region
-                n_poly <- length(poly@polygons[[i]]@Polygons)
-                print(paste("There are",n_poly,"polygons"))
-                # Create progress bar
-                pb <- txtProgressBar(min = 0, max = n_poly, style = 3)
-                for(j in 1:n_poly) {
-                        poly_df <- rbind(poly_df, NA, 
-                                         poly@polygons[[i]]@Polygons[[j]]@coords)
-                        # Update progress bar
-                        setTxtProgressBar(pb, j)
-                }
-                close(pb)
-                print(paste("Finished region",i,"of",n_regions))
-        }
-        poly_df <- data.frame(poly_df)
-        names(poly_df) <- c('lon','lat')
-        return(poly_df)
-}
-
-chile.mun = poly2df(municipalities)
-
-library(ggplot2)
-ggplot() + geom_polygon(data=chile.2, aes(x=lon, y=lat))
-
-
-data(tornados)
-head(as.data.frame(torn))
-
-
-
+ggplot() +  
+        geom_polygon(aes(x=long, y=lat, group=group), fill='grey', size=.05, color='black', data=peru.provinces, alpha=1/2) +
+        theme_bw() +
+        ggtitle("Peru") + 
+        geom_point(data=subset(dat.peru, year>=1900), aes(x=Longitude, y=Latitude, size=Magnitude), color="red", shape=21)
