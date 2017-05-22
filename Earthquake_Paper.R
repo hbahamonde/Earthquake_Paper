b20e896847de06e45f9b438f8b6c925d614bdeea
Hector Bahamonde <bahamondeh@gmail.com>
2017-05-22 14:59:34 -0400

    laptop changes

--- Earthquake_Paper.R
+++ Earthquake_Paper.R
@@ -599,9 +599,9 @@ eq.params.sectoral <- c("b.propagrmanu", "b.Magnitude", "b.p.Population", "b.yea
 
 ## ---- sectoral:model:and:data:does:run ----
 # run the model
-n.iter.sectoral = 200000  # n.iter.sectoral = 200000 // this is for working model
-n.burnin.sectoral = 5000 # n.burnin.sectoral = 5000 // this is for working model
-n.chains.sectoral = 4 # n.chains.sectoral = 4 for the working model
+n.iter.sectoral = 2  # n.iter.sectoral = 200000 // this is for working model
+n.burnin.sectoral = 0 # n.burnin.sectoral = 5000 // this is for working model
+n.chains.sectoral = 1 # n.chains.sectoral = 4 for the working model
 
 earthquakefit.sectoral <- jags(
         data=jags.data.sectoral,
@@ -961,9 +961,9 @@ eq.params.tax <- c("b.Magnitude", "b.p.Population", "b.year", "b.r.long", "b.r.l
 
 ## ---- income:tax:model:and:data:run ----
 # run the model
-n.iter.tax = 200000  # n.iter.tax = 200000 // this is for working model
-n.burnin.tax = 5000 # n.burnin.tax = 5000 // this is for working model
-n.chains.tax = 4 # n.chains.tax = 4 for the working model
+n.iter.tax = 2  # n.iter.tax = 200000 // this is for working model
+n.burnin.tax = 0 # n.burnin.tax = 5000 // this is for working model
+n.chains.tax = 1 # n.chains.tax = 4 for the working model
 
 earthquakefit.tax <- jags(
         data=jags.data.tax,
